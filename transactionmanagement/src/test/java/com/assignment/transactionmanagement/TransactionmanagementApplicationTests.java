2678a20e0906c3cfd2af472f1cb5184bbf2ea34f
Ipek Caglayan <ipekcaglyn@gmail.com>
2024-10-10 20:58:45 +0300

    feat: transaction management service created

---
+++ transactionmanagement/src/test/java/com/assignment/transactionmanagement/TransactionmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.transactionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
