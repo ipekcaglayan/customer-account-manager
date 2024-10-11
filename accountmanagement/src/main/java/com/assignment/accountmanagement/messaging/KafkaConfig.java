7ead7ce31f000a6cce1887c4e3f2ee6eeecf5d85
Ipek Caglayan <ipekcaglyn@gmail.com>
2024-10-11 06:20:10 +0300

    chore: update application YAML and Kafka configurations for container compatibility

--- accountmanagement/src/main/java/com/assignment/accountmanagement/messaging/KafkaConfig.java
+++ accountmanagement/src/main/java/com/assignment/accountmanagement/messaging/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.assignment.accountmanagement.messaging;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -19,11 +20,13 @@ import java.util.Map;
 @EnableKafka
 @RequiredArgsConstructor
 public class KafkaConfig {
+    @Value("${application.kafka.bootstrap-servers}")
+    private String BOOTSTRAP_SERVERS_CONFIG;
 
     @Bean
     public ProducerFactory<String, NotificationDTO> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Change to your Kafka server address
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG); // Change to your Kafka server address
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // Assuming your key is a String
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); // Use custom serializer
 
